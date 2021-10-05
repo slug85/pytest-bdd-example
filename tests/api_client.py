ca9b99405221a544ca5db23603969224ad9478f7
sergey.lugovskoy <meoi971@gmail.com>
2021-10-05 12:51:14 +0300

    логирование запросов

--- tests/api_client.py
+++ tests/api_client.py
@@ -1,14 +1,37 @@
+import logging
+
 import requests
+import http.client as http_client
+from requests import Response
+
+
+def setup_logger():
+    http_client.HTTPConnection.debuglevel = 1
+
+    # You must initialize logging, otherwise you'll not see debug output.
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
 
 
 class APIClient:
 
     def __init__(self, base_address):
         self.base_address = base_address
+        setup_logger()
+        self.headers = {
+            "X-API-Context-Shop-Id": "2527",
+            "X-Data-Platform": "Android",
+            "X-Data-Model": "Samsung",
+            "X-Data-Version": "1.14",
+            "Content-Type": "application/json",
+        }
 
-    def post(self, path="/", params=None, data=None, headers=None):
+    def post(self, path="/", params=None, data=None) -> Response:
         url = self.base_address + path
-        return requests.post(url=url, params=params, data=data, headers=headers)
+        return requests.post(url=url, params=params, data=data, headers=self.headers)
 
-    def get(self, path="/", params=None):
+    def get(self, path="/", params=None) -> Response:
         return requests.get(url=self.base_address + path, params=params)
--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,5 +1,6 @@
 from tests.api_client import APIClient
 
 
-def test_fun(api_client: APIClient):
-    print(api_client.base_address)
+def test_pin(api_client: APIClient):
+    response = api_client.post("mobile/api/v1/oauth/request_token")
+    assert response.status_code == 200
