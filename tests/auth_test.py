f962afc750677c01c80a51c815dd1dde998b6735
sergey.lugovskoy <meoi971@gmail.com>
2021-10-05 12:53:00 +0300

    тест авторизации

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -2,5 +2,12 @@ from tests.api_client import APIClient
 
 
 def test_pin(api_client: APIClient):
-    response = api_client.post("mobile/api/v1/oauth/request_token")
-    assert response.status_code == 200
+    response = api_client.post(path="mobile/api/v1/oauth/request_token", data="""{
+            "grant_type": "phone_code",
+            "client_id": 8,
+            "phone_number": "79002347594",
+            "response_type": "phone_code",
+            "client_secret": "ZlxUY81YlQOdWcof9G6EkHiz0316Z1Xhr0WNHDKY",
+            "scope": ""
+        }""")
+    assert response.status_code == 201
