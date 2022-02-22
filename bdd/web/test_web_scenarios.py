b6a305340159de33ca4f2600434cbf7f403de1f9
sergey.lugovskoy <meoi971@gmail.com>
2022-02-22 14:18:00 +0300

    общий запуск для всех веб тестов

--- bdd/web/test_web_scenarios.py
+++ bdd/web/test_web_scenarios.py
@@ -1,12 +1,10 @@
-from pytest_bdd import scenario
+from pytest_bdd import scenarios
 from web_steps import *
 
 
-@scenario(feature_name="features/web.feature", scenario_name="Открытие формы")
-def test_auth(datastore: DataStore):
-    pass
+# @scenario(feature_name="features/web.feature", scenario_name="Открытие формы")
+# def test_auth(datastore: DataStore):
+#     pass
 
 
-@scenario(feature_name="features/web.feature", scenario_name="Открытие каталога")
-def test_catalog(datastore: DataStore):
-    pass
+scenarios("./features")
