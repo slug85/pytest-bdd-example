537d3bc7c26ec7f6b22bb82e579ed861bafb436a
sergey.lugovskoy <meoi971@gmail.com>
2022-03-04 09:55:32 +0300

    remote

--- bdd/web/conftest.py
+++ bdd/web/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 import selene.browser
 from _pytest.fixtures import FixtureRequest
-from selene import config
+from selene import config, browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -9,6 +10,14 @@ def config_browser(request: FixtureRequest):
     base_url = request.config.getoption("--url")
     config.base_url = base_url
     config.timeout = 20
+
+    driver = webdriver.Remote(
+        command_executor='http://selenosis.selenosis-dev.svc.k8s-test.dataspace:8080/wd/hub',
+        desired_capabilities={'browserName': 'chrome',
+                              'enableVNC': True,
+                              'javascriptEnabled': True})
+
+    browser.set_driver(driver)
     yield
 
 
